077a541de9a76dda0e8c976cd4bf971c666e01ca
GuilianoRangel <guiliano@gmail.com>
2021-08-24 00:46:41 -0300

    Versao inicial

---
+++ src/main/java/br/ueg/modelo/application/AppStartupRunner.java
@@ -0,0 +1,165 @@
+package br.ueg.modelo.application;
+
+import br.ueg.modelo.application.enums.StatusAtivoInativo;
+import br.ueg.modelo.application.enums.StatusSimNao;
+import br.ueg.modelo.application.model.*;
+import br.ueg.modelo.application.repository.GrupoRepository;
+import br.ueg.modelo.application.repository.ModuloRepository;
+import br.ueg.modelo.application.repository.UsuarioRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Classe para ser executado após o Spring Inicializar.
+ * Verificado o modo de start para spring.jpa.hibernate.ddl-auto==create-drop
+ */
+
+@Component
+public class AppStartupRunner implements ApplicationRunner {
+
+    public static final String NONE="none";
+    public static final String CREATE_DROP="create-drop";
+
+    private static final Logger LOG =
+            LoggerFactory.getLogger(AppStartupRunner.class);
+
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String ddlAuto;
+
+    @Autowired
+    ModuloRepository moduloRepository;
+
+    @Autowired
+    GrupoRepository grupoRepository;
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        LOG.info("Application started with option names : {}",
+                args.getOptionNames());
+        LOG.info("spring.jpa.hibernate.ddl-auto={}",ddlAuto);
+
+        if(this.ddlAuto.equals(CREATE_DROP.trim())){
+            this.initiateDemoInstance();
+        }
+    }
+
+    private void initiateDemoInstance() {
+        LOG.info("initiateDemoInstance");
+        Modulo moduloUsuario = createModuloCrud("USUARIO", "Manter Usuário");
+
+        Modulo moduloGrupo = createModuloCrud("GRUPO", "Manter Grupo");
+
+        Grupo grupo = createGrupoAdmin(moduloUsuario, moduloGrupo);
+
+        Usuario usuario = new Usuario();
+        usuario.setStatus(StatusAtivoInativo.ATIVO);
+        usuario.setDataCadastrado(LocalDate.now());
+        usuario.setDataAtualizado(LocalDate.now());
+        usuario.setTelefones(new HashSet<>());
+        usuario.setCpf("10010010017");
+        usuario.setLogin("admin");
+        usuario.setNome("Administrador");
+        usuario.setEmail("admin@teste.com.br");
+        usuario.setSenha(new BCryptPasswordEncoder().encode("admin"));
+
+        usuario = usuarioRepository.save(usuario);
+
+        Set<UsuarioGrupo> usuarioGrupos = new HashSet<>();
+        usuarioGrupos.add(new UsuarioGrupo(null,usuario,grupo));
+        usuario.setGrupos(usuarioGrupos);
+        usuario = usuarioRepository.save(usuario);
+    }
+
+    private Grupo createGrupoAdmin(Modulo moduloUsuario, Modulo moduloGrupo) {
+        Grupo grupo = new Grupo();
+        grupo.setNome("Administradores");
+        grupo.setAdministrador(StatusSimNao.SIM);
+        grupo.setDescricao("Grupo de Administradores");
+        grupo.setStatus(StatusAtivoInativo.ATIVO);
+        grupo = grupoRepository.save(grupo);
+        final Grupo lGrupo = grupo;
+        grupo.setGrupoFuncionalidades(moduloUsuario.getFuncionalidades().stream().map(
+                (funcionalidade) ->
+                        new GrupoFuncionalidade(null, lGrupo,funcionalidade)).collect(Collectors.toSet())
+        );
+        grupo.getGrupoFuncionalidades().addAll(moduloGrupo.getFuncionalidades().stream().map(
+                (funcionalidade) ->
+                        new GrupoFuncionalidade(null, lGrupo, funcionalidade)).collect(Collectors.toSet())
+        );
+
+        grupoRepository.save(grupo);
+        return grupo;
+    }
+
+    private Modulo createModuloCrud(String moduloMNemonico, String moduloNome) {
+        Modulo moduloUsuario = new Modulo();
+
+        moduloUsuario.setMnemonico(moduloMNemonico);
+        moduloUsuario.setNome(moduloNome);
+        moduloUsuario.setStatus(StatusAtivoInativo.ATIVO);
+        moduloUsuario = moduloRepository.save(moduloUsuario);
+
+        final Modulo lModuloUsuario = moduloUsuario;
+        moduloUsuario.setFuncionalidades(
+                getFuncionalidadesCrud().stream().map(funcionalidade -> {
+                    funcionalidade.setModulo(lModuloUsuario);
+                    return funcionalidade;
+                }).collect(Collectors.toSet()));
+        moduloUsuario = moduloRepository.save(moduloUsuario);
+        return moduloUsuario;
+    }
+
+    /**
+     * retorna Funcionalidades padrão de um CRRUD
+     * @return
+     */
+    private Set<Funcionalidade> getFuncionalidadesCrud() {
+        Set<Funcionalidade> funcionalidades = new HashSet<>();
+
+        Funcionalidade fManter = new Funcionalidade();
+        fManter.setMnemonico("INCLUIR");
+        fManter.setNome("Incluir");
+        fManter.setStatus(StatusAtivoInativo.ATIVO);
+        funcionalidades.add(fManter);
+
+        fManter = new Funcionalidade();
+        fManter.setMnemonico("ALTERAR");
+        fManter.setNome("Alterar");
+        fManter.setStatus(StatusAtivoInativo.ATIVO);
+        funcionalidades.add(fManter);
+
+        fManter = new Funcionalidade();
+        fManter.setMnemonico("ATIVAR_INATIVAR");
+        fManter.setNome("Ativar/Inativar");
+        fManter.setStatus(StatusAtivoInativo.ATIVO);
+        funcionalidades.add(fManter);
+
+        fManter = new Funcionalidade();
+        fManter.setMnemonico("PESQUISAR");
+        fManter.setNome("Pesquisar");
+        fManter.setStatus(StatusAtivoInativo.ATIVO);
+        funcionalidades.add(fManter);
+
+        fManter = new Funcionalidade();
+        fManter.setMnemonico("VISUALIZAR");
+        fManter.setNome("Visualizar");
+        fManter.setStatus(StatusAtivoInativo.ATIVO);
+        funcionalidades.add(fManter);
+        return funcionalidades;
+    }
+}
